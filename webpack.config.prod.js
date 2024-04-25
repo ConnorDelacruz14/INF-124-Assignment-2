cacc68d92a4ab6017140757c6d47fc0df90ef784
Connor Delacruz <ConnorD1432@gmail.com>
2024-04-25 11:15:40 -0700

    navbar done

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -19,7 +19,6 @@ module.exports = merge(common, {
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
   ],
